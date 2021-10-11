e599f2a3328282029e844b6307463c193f47c610
warp89 <warp8911221122@gmail.com>
2021-10-11 13:59:46 +0300

    Add homework

--- Assets/HomeWork.cs
+++ Assets/HomeWork.cs
@@ -4,15 +4,43 @@ using UnityEngine;
 
 public class HomeWork : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private int arrayLenght;
     void Start()
     {
-        
+        int[] arrayToSort = CreateArray(arrayLenght);
+        for (int i = 0; i < arrayToSort.Length; i++)
+        {
+            int minimumNumberIndex = i;
+            for (int j = i; j < arrayToSort.Length; j++)
+            {
+                if (arrayToSort[minimumNumberIndex] > arrayToSort[j])
+                {
+                    minimumNumberIndex = j;
+                }
+            }
+
+            int temp = arrayToSort[i];
+            arrayToSort[i] = arrayToSort[minimumNumberIndex];
+            arrayToSort[minimumNumberIndex] = temp;
+        }
+        PrintArray(arrayToSort);
     }
 
-    // Update is called once per frame
-    void Update()
+    private int[] CreateArray(int arrayLenght)
     {
-        
+        int[] array = new int[arrayLenght];
+        for (int i = 0; i < arrayLenght; i++)
+        {
+            array[i] = Random.Range(-10, 11);
+        }
+        return array;
+    }
+    private void PrintArray(int[] arrayToPrint)
+    {
+        for (int i = 0; i < arrayToPrint.Length; i++)
+        {
+            Debug.Log(arrayToPrint[i]);
+        }
     }
 }
