e3f6ae6c7d50dd9ae16a94e3270c4ae3fc9a51d7
warp89 <warp8911221122@gmail.com>
2021-10-21 13:15:20 +0300

    Add timer

--- Assets/HomeWork.cs
+++ Assets/HomeWork.cs
@@ -1,14 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class HomeWork : MonoBehaviour
 {
     [SerializeField]
     private int arrayLenght;
+    [SerializeField]
+    private int rangeFrom = -100;
+    [SerializeField]
+    private int rangeTo = 101;
     void Start()
     {
+
         int[] arrayToSort = CreateArray(arrayLenght);
+        PrintArray(arrayToSort);
+        Debug.Log("Array before sort ^ Array after sort v");
+        DateTime startTime = DateTime.Now;
+        SelectionSort(arrayToSort);
+        PrintArray(arrayToSort);
+        DateTime endTime = DateTime.Now;
+        Debug.Log($"Sorted in {(endTime - startTime).TotalSeconds} seconds");
+    }
+
+    private int[] CreateArray(int arrayLenght)
+    {
+        int[] array = new int[arrayLenght];
+        for (int i = 0; i < arrayLenght; i++)
+        {
+            array[i] = UnityEngine.Random.Range(rangeFrom, rangeTo);
+        }
+        return array;
+    }
+    private void PrintArray(int[] arrayToPrint)
+    {
+        for (int i = 0; i < arrayToPrint.Length; i++)
+        {
+            Debug.Log(arrayToPrint[i]);
+        }
+    }
+    private void SelectionSort(int[] arrayToSort)
+    {
         for (int i = 0; i < arrayToSort.Length; i++)
         {
             int minimumNumberIndex = i;
@@ -23,24 +56,6 @@ public class HomeWork : MonoBehaviour
             int temp = arrayToSort[i];
             arrayToSort[i] = arrayToSort[minimumNumberIndex];
             arrayToSort[minimumNumberIndex] = temp;
-        }
-        PrintArray(arrayToSort);
-    }
-
-    private int[] CreateArray(int arrayLenght)
-    {
-        int[] array = new int[arrayLenght];
-        for (int i = 0; i < arrayLenght; i++)
-        {
-            array[i] = Random.Range(-10, 11);
-        }
-        return array;
-    }
-    private void PrintArray(int[] arrayToPrint)
-    {
-        for (int i = 0; i < arrayToPrint.Length; i++)
-        {
-            Debug.Log(arrayToPrint[i]);
         }
     }
 }
